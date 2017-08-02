0ec8f2b4697455c60e72208eba3dc731ffbc0287
Alexander Pushkarev <axp-dev@yandex.com>
2017-08-02 18:54:04 +0500

    Initial commit

---
+++ src/CinemaPark.php
@@ -0,0 +1,48 @@
+<?php
+namespace AXP\CinemaPark;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+
+/**
+ * Class CinemaPark
+ *
+ * @author  Alexander Pushkarev <axp-dev@yandex.com>
+ * @link    https://github.com/axp-dev/cinemapark-api
+ * @package AXP\CinemaPark
+ */
+
+class CinemaPark
+{
+    /**
+     * Список конечных url api
+     *
+     * @var array
+     */
+    private $endpoints = [
+        'information' => 'http://json.integration.www.cinemapark.ru/',
+        'booking'     => 'http://api.booking.www.cinemapark.ru/'
+    ];
+
+    /**
+     * Запрос к API
+     *
+     * @param string $url
+     *
+     * @return mixed
+     * @throws CinemaParkException
+     *
+     */
+    protected function query($url)
+    {
+        try {
+            $client = new GuzzleClient();
+            $response = $client->request('GET', $url);
+            $result = json_decode($response->getBody(), true);
+
+            return $result;
+        } catch (ClientException $e) {
+            throw new CinemaParkException($e->getMessage());
+        }
+    }
+}
---
+++ src/CinemaParkException.php
@@ -0,0 +1,16 @@
+<?php
+namespace AXP\CinemaPark;
+
+use Exception;
+
+/**
+ * Class CinemaParkException
+ *
+ * @author  Alexander Pushkarev <axp-dev@yandex.com>
+ * @link    https://github.com/axp-dev/cinemapark-api
+ * @package AXP\CinemaPark
+ */
+class CinemaParkException extends Exception
+{
+
+}
